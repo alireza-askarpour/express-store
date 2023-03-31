4d262d0f432df5844e3146af876a8a188ebfdf27
Alireza Askarpour <askarpourdev@gmail.com>
2023-03-31 07:28:24 -0400

    feat: add delete file utils

---
+++ src/utils/deleteFile.utils.js
@@ -0,0 +1,22 @@
+import fs from 'fs'
+import path from 'path'
+import { __dirname } from '../../__dirname.js'
+
+export const deleteFile = async fileAddress => {
+  const pathFiles = Array.isArray(fileAddress)
+    ? fileAddress.map(address => path.join(__dirname, address))
+    : [path.join(__dirname, fileAddress)]
+
+  const deletePromises = pathFiles.map(
+    pathFile =>
+      new Promise(resolve => {
+        if (fs.existsSync(pathFile)) {
+          fs.unlink(pathFile, err => {})
+        }
+        resolve()
+      })
+  )
+
+  await Promise.all(deletePromises)
+  console.log(`All files were deleted`)
+}
